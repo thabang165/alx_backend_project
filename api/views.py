c1a45ac462b1f275a8e2aa02f76337bc836a2d44
thabang165 <atlemola@gmail.com>
2025-10-05 19:45:21 +0200

    feat: implement CRUD viewsets for Product, Store, and Price

--- api/views.py
+++ api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Product, Store, Price
+from .serializers import ProductSerializer, StoreSerializer, PriceSerializer
 
-# Create your views here.
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class StoreViewSet(viewsets.ModelViewSet):
+    queryset = Store.objects.all()
+    serializer_class = StoreSerializer
+
+
+class PriceViewSet(viewsets.ModelViewSet):
+    queryset = Price.objects.all()
+    serializer_class = PriceSerializer
+
