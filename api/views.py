e40ce16b790326677bd48bcf26114bb7daffbbf2
thabang165 <atlemola@gmail.com>
2025-10-12 20:54:54 +0200

    create API views for Product and PriceHistory

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
-from .models import Product, Store, Price
-from .serializers import ProductSerializer, StoreSerializer, PriceSerializer
+from .models import Product, Store, Price, PriceHistory
+from .serializers import ProductSerializer, StoreSerializer, PriceSerializer, PriceHistorySerializer
+from rest_framework import generics
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
@@ -16,3 +17,19 @@ class PriceViewSet(viewsets.ModelViewSet):
     queryset = Price.objects.all()
     serializer_class = PriceSerializer
 
+class ProductListCreateView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+# PriceHistory
+class PriceHistoryListCreateView(generics.ListCreateAPIView):
+    queryset = PriceHistory.objects.all()
+    serializer_class = PriceHistorySerializer
+
+class PriceHistoryDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = PriceHistory.objects.all()
+    serializer_class = PriceHistorySerializer
