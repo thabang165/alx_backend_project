affa8d4c2ad55ba974964768b2b15df1762eec5f
thabang165 <atlemola@gmail.com>
2025-10-12 20:57:54 +0200

    setup API urls for Product and PriceHistory endpoints

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ProductViewSet, StoreViewSet, PriceViewSet
+from django.urls import path
+from .views import (
+    ProductListCreateView, ProductDetailView,
+    PriceHistoryListCreateView, PriceHistoryDetailView
+)
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet)
@@ -9,4 +14,6 @@ router.register(r'prices', PriceViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
 ]
